3bdb28127510e22de8900d670238b534af0339d8
Drogshell <89346155+Drogshell@users.noreply.github.com>
2025-07-20 17:51:16 +1000

    Setting up Chunks
    - Added a small bit of code to spawn the infinite plane the player will run on
    - Updated MainLevel Scene

--- Assets/Scripts/LevelGen.cs
+++ Assets/Scripts/LevelGen.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 public class LevelGen : MonoBehaviour
 {
     [SerializeField] private GameObject chunkPrefab;
-
+    [SerializeField] private int startingChunkSize = 12;
+    [SerializeField] private Transform chunkParent;
+    [SerializeField] private float chunkLength = 10f;
+    
     private void Start()
     {
-        Instantiate(chunkPrefab, transform.position, Quaternion.identity);
+        for (int i = 0; i < startingChunkSize; i++)
+        {
+            var spawnPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + chunkLength * i);
+            Instantiate(chunkPrefab, spawnPosition, Quaternion.identity, chunkParent);
+        }
     }
     
 }
