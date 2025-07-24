3d99c62f5a14f1ca0f54ddc2191d5f03c12e9e5b
Drogshell <89346155+Drogshell@users.noreply.github.com>
2025-07-24 17:01:47 +1000

    Added in assets used for world building
    - Added in a bunch of assets that fit the theme
    - Updated the level gen script to use these assets and spawn platforms under the players feet

--- Assets/Scripts/LevelGen.cs
+++ Assets/Scripts/LevelGen.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGen : MonoBehaviour
@@ -6,14 +8,78 @@ public class LevelGen : MonoBehaviour
     [SerializeField] private int startingChunkSize = 12;
     [SerializeField] private Transform chunkParent;
     [SerializeField] private float chunkLength = 10f;
+    [SerializeField] private float moveSpeed = 2f;
+
+    private List<GameObject> _chunks = new();
     
     private void Start()
     {
-        for (int i = 0; i < startingChunkSize; i++)
+        SpawnInitialChunks();
+    }
+
+    private void Update()
+    {
+        MoveChunks();
+    }
+
+    private void SpawnInitialChunks()
+    {
+        for (var i = 0; i < startingChunkSize; i++)
         {
-            var spawnPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + chunkLength * i);
-            Instantiate(chunkPrefab, spawnPosition, Quaternion.identity, chunkParent);
+            SpawnSingleChunk();
         }
+    }
+
+    private void SpawnSingleChunk()
+    {
+        var spawnPositionZ = CalculateSpawnPoint();
+        
+        var chunkSpawnPosition = new Vector3(transform.position.x, transform.position.y, spawnPositionZ);
+        var chunk = Instantiate(chunkPrefab, chunkSpawnPosition, Quaternion.identity, chunkParent);
+            
+        _chunks.Add(chunk);
+    }
+
+    private float CalculateSpawnPoint()
+    {
+        float spawnPositionZ;
+
+        if (_chunks.Count == 0)
+        {
+            spawnPositionZ = transform.position.z;
+        }
+        else
+        {
+            // Get the specific transform of the last element in the list
+            spawnPositionZ = _chunks[^1].transform.position.z + chunkLength;
+        }
+
+        return spawnPositionZ;
+    }
+
+    private void MoveChunks()
+    {
+        for (var i = 0; i < _chunks.Count; i++)
+        {
+            // Grab the chunk and move it backwards
+            var chunk = _chunks[i];
+            chunk.transform.Translate(-transform.forward * (moveSpeed * Time.deltaTime));
+            
+            // If the chunk moves behind the player delete it
+            if (chunk.transform.position.z < Camera.main.transform.position.z - chunkLength)
+            {
+                DestroyChunks(chunk);
+            }
+            
+        }
+        
+    }
+
+    private void DestroyChunks(GameObject chunk)
+    {
+        _chunks.Remove(chunk);
+        Destroy(chunk);
+        SpawnSingleChunk();
     }
     
 }
